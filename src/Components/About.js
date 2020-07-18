964cc9fb53e7366bfe67e62434f0ae0f4fc869c4
Fahad Shaikh <linkfahad@hotmail.com>
2020-07-18 13:33:49 +0500

    added sample data

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,7 +3,28 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      About
+      <p> Since 1942 Nike has been rendering its services to its valued customers by offering quality products. </p>
+
+<p>It was incorporated in as Nike Shoe Company () Limited in 1951 and went public to become Nike Limited in the year 1979. </p>
+
+<p>Since its inception, the company has not only maintained a good reputation of manufacturing high quality footwear for all segments but has also been designing shoes in accordance with the changing fashions and trends. </p>
+
+<p>If you encounter any problem with our product, please refer to the store of purchase with the original cash memo. The store manager would be able to assist you.</p>
+
+<p>Nike is serving its valued customers through a strong retail network comprising of more than 431 retail outlets, 544 registered wholesale dealers and 136 DSP franchise served by 12 wholesale depots across the country.</p>
+
+<p>Besides catering local market, Nike also shows its presence in an international footwear market through its export department which is constantly exploring new potential market in order to earn foreign exchange.</p>
+
+<p>Being a multinational company Nike has played a vital role in the economic progress of . It has introduced sophisticated technology and business skills to the country and provided direct and indirect employment to about 10,000 people. Along with its own manufacturing capacity Nike is also outsourcing its products nationally and internationally to meet the demands of its valued customers.</p>
+
+<p>As the Corporate social responsibility is an integral part of every business and Nike fulfills its CSR by patronizing various charitable organizations and also takes part in rehabilitation of society during the natural disasters.</p>
+
+<p>Moreover Nike is also operating an international program under the name of Nike Children program, which aims to create a brighter future for the children of the community in which we operate. Under this program Nike encourages and supports under privileged children by providing shoes, donations and also arranging different healthy activities for them which add values in their lives.</p>
+
+<p>Nike has always been the market leader and in order to maintain its leadership it has invested millions of rupees in updating its systems and equipment during the last few years. This will enable the company to expand, modernize and develop its operations and in the process provide additional employment opportunities.</p>
+
+<p>Nike the market leader is well equipped to cater the customer’s demands and to meet future challenges.</p>
+      
     </div>
   )
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,16 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      Home
+      
+<h1>We are committed to ensuring your satisfaction with any merchandise you have ordered from us. However, if you’re unhappy for any reason, here’s what to do. </h1>
+
+<p>
+For exchange - please contact our customer care department via WhatsApp number/email mentioned above.
+Items once purchased are non-refundable.
+In case of Items purchased in Sale – Please note that No return/refund/exchange is allowed.
+You may exchange the size of the delivered goods ( If advised by the Customer Services Team Member only) at the nearest Company’s outlets on production of original invoice provided the goods are unused, must be packed in their original packing with all tags attached, unworn/undamaged condition and not altered.
+Store manager may refuse to accept any exchange at its discretion if he does not possess required goods.
+The decision of the Company would be considered final in all cases. </p>
     </div>
   )
 }
